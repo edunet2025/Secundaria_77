59a740fd1419f7ca91d99a1dc92c8dd0b334b17f
secundariarepublicadepanama77 <atecnologias.77@gmail.com>
2025-06-08 14:58:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
 // server.js (mínimo para login con Supabase)
 const express = require("express");
-const cors = require("cors");
-const { createClient } = require("@supabase/supabase-js");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-const supabase = createClient(
-  "https://mhnzjhelbupyifdlpngv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1obnpqaGVsYnVweWlmZGxwbmd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTg1MjUsImV4cCI6MjA2NDc5NDUyNX0.tZAtvUL6kAFfEhwrXgopbQLcnq9qCCm5zpPBkm6z8wY"
-);
-
 app.use(cors());
 app.use(express.json());
 
@@ -38,17 +27,10 @@ app.post("/api/login", async (req, res) => {
   const usuarioLogeado = data[0];
   res.json({
     mensaje: "Inicio de sesión exitoso",
-    tipo: usuarioLogeado.tipo,     // admin, docente, alumno
-    nombre: usuarioLogeado.nombre  // puedes personalizar más datos si quieres
+    tipo: usuarioLogeado.tipo,
+    nombre: usuarioLogeado.nombre
   });
 });
-  const usuarioEncontrado = data[0];
-
-  res.json({
-    mensaje: "Inicio de sesión exitoso",
-    tipo: usuarioEncontrado.tipo,
-    nombre: usuarioEncontrado.nombre
-  });
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
